b4035a6c8c4a8a9830b4ed27726e02e238a93563
srivastavarahul442 <srivastavarahul442@gmail.com>
2024-12-17 21:49:49 +0530

    Learning routing/ express--->next()

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
-
 const express = require("express");
 
 const app = express();
 
-app.get("/user",(req,res)=>{
-    console.log(req.query)
-    res.send({firstName:"Rahul",lastName:"Kumar"})
-})
+/**
+ * app.use("/route", rH1, rH2, rH3, rH4, rH5)
+ * app.use("/route", [rH1, rH2, rH3, rH4, rH5])
+ * app.use("/route", [rH1, rH2], rH3, rH4, rH5)
+ * 
+ * it will work same for get post delete...
+ */
 
+app.use(
+  "/user",
+  //1st route handler function
+  (req, res,next) => {
+    next();
+    // res.send("1st route handler")
+    console.log("1st")
+    
+  },
+  //2nd route handler function
+  (req,res,next) => {
+    // res.send("2nd route handler");
+    next();
+    console.log("2nd")
+  },
+  //2nd route handler function
+  (req,res,next)=>{
+    res.send("3rd route handler")
+    // next();
+    console.log("3rd")
+  }
+);
 
-app.listen(7777,()=>{
-    console.log("Server is successfully listining")
-})
+app.listen(7777, () => {
+  console.log("Server is successfully listining");
+});
