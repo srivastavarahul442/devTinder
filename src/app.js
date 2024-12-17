790d904c859089407f98c49acae9eb80e7e895e7
srivastavarahul442 <srivastavarahul442@gmail.com>
2024-12-17 19:47:37 +0530

    Learning GET POST DELETE

--- src/app.js
+++ src/app.js
@@ -3,23 +3,24 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Rahul",lastName:"Kumar"})
+})
 
+app.post("/user",(req,res)=>{
+    //save the data to DB
+    res.send("Saved the data to database")
+})
 
-app.use("/test",(req,res)=>{
+app.delete("/user",(req,res)=>{
+    //save the data to DB
+    res.send("data is deleted")
+})
+
+app.use("/user",(req,res)=>{
     res.send("Hello from the test");
 })
 
-app.use("/home",(req,res)=>{
-    res.send("Hello from the home");
-})
-
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello");
-})
-
-app.use("/",(req,res)=>{
-    res.send("Hello from the /");
-})
 
 app.listen(7777,()=>{
     console.log("Server is successfully listining")
